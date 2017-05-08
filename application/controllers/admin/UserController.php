8d3683bf14b904020780cbd011970fc4998b4955
rainsun007 <sunqingyu_@163.com>
2017-05-08 14:15:51 +0800

    bug fix

--- application/controllers/admin/UserController.php
+++ application/controllers/admin/UserController.php
@@ -103,7 +103,7 @@ class UserController extends AdminController
 
         $data = array(
           'username' => $name,
-          'status' => DyRequest::postStr('status') == 'true' ? 1 : 0,
+          'status' => $this->userId == 1 ? 1 : (DyRequest::postStr('status') == 'true' ? 1 : 0),
           'role_ids' => trim(DyRequest::postStr('roles'), ','),
         );
         if (DyRequest::postStr('password')) {
